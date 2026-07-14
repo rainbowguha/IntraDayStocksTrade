8ec94b8e332cf34584fce640db24f5184199d41a
rainbowguha <rainbowguha@gmail.com>
2026-07-14 20:53:47 +0530

    Delete Credit.py

--- Credit.py
+++
@@ -1,3 +0,0 @@
-# github cron
-git_access_token = 'Bearer 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'
-cron_url = 'https://api.github.com/repos/tapasguha1990/streamlit/actions/workflows/main.yml/dispatches'
